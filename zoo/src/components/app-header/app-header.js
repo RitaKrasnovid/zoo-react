a7e949a02cecb26475ba966bd1b2367a2d8ed73e
RitaKrasnovid <mkrasnovid@lineate.com>
2020-03-09 23:54:35 +0400

    Import scss files to headers, use scss variables

--- zoo/src/components/app-header/app-header.js
+++ zoo/src/components/app-header/app-header.js
@@ -7,20 +7,20 @@ const AppHeader = () => {
     <header className="app-header">
       <nav className="app-header_nav">
         <div className="app-header__logo">
-          <a className="app-header__logo--nav-home" href="#">Super zoo</a>
+          <a className="app-header__logo--nav-home" href="/">Super zoo</a>
         </div>
         <ul className="app-header__list">
           <li className="app-header__item">
-            <a className="app-header__nav-link" href="#">Home</a>
+            <a className="app-header__nav-link" href="/">Home</a>
           </li>
           <li className="app-header__item">
-            <a className="app-header__nav-link" href="#">Animals</a>
+            <a className="app-header__nav-link" href="/">Animals</a>
           </li>
           <li className="app-header__item">
-            <a className="app-header__nav-link" href="#">News</a>
+            <a className="app-header__nav-link" href="/">News</a>
           </li>
           <li className="app-header__item">
-            <a className="app-header__nav-link" href="#">About Zoo</a>
+            <a className="app-header__nav-link" href="/">About Zoo</a>
           </li>
         </ul>
       </nav>
