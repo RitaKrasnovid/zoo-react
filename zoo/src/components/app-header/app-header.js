0e72a6963a09bc4ba9b258c7df6222f2cf4f1e23
RitaKrasnovid <mkrasnovid@lineate.com>
2020-03-20 20:49:09 +0400

    Create hamburger component

--- zoo/src/components/app-header/app-header.js
+++ zoo/src/components/app-header/app-header.js
@@ -1,31 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Hamburger from '../hamburger';
 
 import './app-header.scss';
 
-const AppHeader = () => {
-  return (
-    <header className="app-header">
-      <nav className="app-header_nav">
-        <div className="app-header__logo">
-          <a className="app-header__logo--nav-home" href="/">Super zoo</a>
-        </div>
-        <ul className="app-header__list">
-          <li className="app-header__item">
-            <a className="app-header__nav-link" href="/">Home</a>
-          </li>
-          <li className="app-header__item">
-            <a className="app-header__nav-link" href="/">Animals</a>
-          </li>
-          <li className="app-header__item">
-            <a className="app-header__nav-link" href="/">News</a>
-          </li>
-          <li className="app-header__item">
-            <a className="app-header__nav-link" href="/">About Zoo</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
-}
+export default class AppHeader extends Component {
+  state = {
+    show: false,
+  }
 
-export default AppHeader;
+  onToggleNav = () => {
+    this.setState(({ show }) => {
+      return {
+        show: !show,
+      }
+    });
+  };
+
+  render() {
+    const { show } = this.state;
+
+    return (
+      <header className='app-header'>
+        <nav className='app-header__nav'>
+          <div className='app-header__logo'>
+            <a className='app-header__logo--nav-home' href='/'>
+              Super zoo
+            </a>
+          </div>
+          <ul className={`app-header__list ${show ? 'show' : ''}`}>
+            <li className='app-header__item'>
+              <a className='app-header__nav-link' href='/'>
+                Home
+              </a>
+            </li>
+            <li className='app-header__item'>
+              <a className='app-header__nav-link' href='/'>
+                Animals
+              </a>
+            </li>
+            <li className='app-header__item'>
+              <a className='app-header__nav-link' href='/'>
+                News
+              </a>
+            </li>
+            <li className='app-header__item'>
+              <a className='app-header__nav-link' href='/'>
+                About Zoo
+              </a>
+            </li>
+          </ul>
+          <Hamburger onToggleNav={this.onToggleNav} show={show} />
+        </nav>
+      </header>
+    );
+  }
+}
