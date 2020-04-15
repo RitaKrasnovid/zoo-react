83d72e51ed51c54cde874567678c50c24d5d99e1
RitaKrasnovid <mkrasnovid@lineate.com>
2020-04-15 17:38:10 +0400

    Create card component

--- zoo/src/pages/animals/animals.js
+++ zoo/src/pages/animals/animals.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 
+import Card from '../../components/card';
+
 export default class AnimalsPage extends Component {
   render() {
     return(
       <div className="animals">
         Animals page
+        <Card />
       </div>
     );
   }
