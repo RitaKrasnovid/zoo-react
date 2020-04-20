3494a4da521273a41ef652a259e91f32c2c2208c
RitaKrasnovid <mkrasnovid@lineate.com>
2020-04-20 14:02:53 +0400

    Add default image

--- zoo/src/components/card/card.js
+++ zoo/src/components/card/card.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./card.scss";
 
-const Card = ({id, name}) => {
-  return (
-    <Link className="card" to={`/animals/${id}`}>
-      <img src="./images/tiger.jpg" alt={name} className="card__image" />
-      <header className="card__title">{name}</header>
-    </Link>
-  );
-};
+export default class Card extends Component {
+  state = {
+    imageUrl: './images/default-image.png',
+  };
 
-export default Card;
+  constructor() {
+    super();
+
+  }
+
+  // if (images[0].data.data) {
+  //   const blob = new Blob(images[0].data.data, { type: 'image/jpg' });
+
+  //   const reader = new FileReader();
+  //   reader.readAsDataURL(blob);
+
+  //   reader.onload = () => {
+  //     this.setState(({imageUrl}) => {
+  //       return {
+  //         imageUrl: reader.result,
+  //       }
+  //     });
+  //   };
+  // }
+
+  render() {
+    const { imageUrl } = this.state;
+    const { id, name } = this.props;
+
+    return (
+      <Link className="card" to={`/animals/${id}`}>
+        <img src={imageUrl} alt={name} className="card__image" id="image" />
+        <header className="card__title">{name}</header>
+      </Link>
+    );
+  }
+}
+
--- zoo/src/pages/animals/animals.js
+++ zoo/src/pages/animals/animals.js
@@ -41,19 +41,19 @@ export default class AnimalsPage extends Component {
         <SectionLayout>
           <h1 className="animals__title">Animals of our ZOO</h1>
           <header className="animals__sectionHeader">
-            <div className="animals__headerButton" onClick={ ()=>console.log('bird') }>
+            <div className="animals__headerButton" onClick={() => console.log('bird')}>
               <RoundButton name={"bird"} />
               Birds
             </div>
-            <div className="animals__headerButton" onClick={ ()=>console.log('mammal') }>
+            <div className="animals__headerButton" onClick={() => console.log('mammal')}>
               <RoundButton name={"mammal"} />
               Mammals
             </div>
-            <div className="animals__headerButton" onClick={ ()=>console.log('fish') }>
+            <div className="animals__headerButton" onClick={() => console.log('fish')}>
               <RoundButton name={"fish"} />
               Fish
             </div>
-            <div className="animals__headerButton" onClick={ ()=>console.log('reptile') }>
+            <div className="animals__headerButton" onClick={() => console.log('reptile')}>
               <RoundButton name={"reptile"} />
               Reptiles
             </div>
