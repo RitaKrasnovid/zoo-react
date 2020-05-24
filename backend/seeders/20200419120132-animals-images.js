ea24155f939c577769c5297c4d27191675801108
RitaKrasnovid <mkrasnovid@lineate.com>
2020-05-24 19:27:53 +0400

    Add animal images

--- backend/seeders/20200419120132-animals-images.js
+++ backend/seeders/20200419120132-animals-images.js
@@ -14,6 +14,30 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date(),
       },
+      {
+        animalId: 3,
+        imageId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        animalId: 4,
+        imageId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        animalId: 6,
+        imageId: 5,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        animalId: 7,
+        imageId: 6,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
     ], {});
   },
 
