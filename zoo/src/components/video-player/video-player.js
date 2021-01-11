1ad091669b646f5feb76b1373dc6a5d8a4fa135c
RitaKrasnovid <mkrasnovid@lineate.com>
2021-01-11 14:18:46 +0400

    Crate video player component

---
+++ zoo/src/components/video-player/video-player.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import YouTube from 'react-youtube';
+
+class YouTubeVideo extends Component {
+  onReady(event) {
+    event.target.pauseVideo();
+  }
+
+  render = () => {
+    const { id } = this.props;
+    const opts = {
+      playerVars: {
+        autoplay: 1,
+        modestbranding: 1,
+        showinfo: 0,
+      },
+    };
+
+    return (
+      <div>
+        <YouTube videoId={id} opts={opts} onReady={this.onReady} />
+      </div>
+    );
+  };
+}
+
+export default YouTubeVideo;
