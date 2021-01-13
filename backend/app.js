3a927de8fc30338f7e6599f7f019ecea7f4bdfac
RitaKrasnovid <mkrasnovid@lineate.com>
2021-01-13 11:55:51 +0400

    Set up google keys

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const passport = require('passport');
+const passportSetup = require('./config/passport-setup');
+const cookieSession = require('cookie-session');
+const keys = require("./config/keys");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:3000",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(cookieSession({
+  // milliseconds of a day
+  maxAge: 24*60*60*1000,
+  keys:[keys.COOKIE_KEY],
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routers')(app);
 
---
+++ backend/config/keys.js
@@ -0,0 +1,13 @@
+const GOOGLE_TOKEN = {
+  CLIENT_ID: '189500692755-4nnlqlvge3lq0lqobi54ds85l0rosdq4.apps.googleusercontent.com',
+  CLIENT_SECRET: '24Qq2KF77ORmWFjjdY2OcbUo',
+};
+
+const SESSION = {
+  COOKIE_KEY: "thisappisawesomecookiekey"
+};
+
+module.exports = {
+  ...GOOGLE_TOKEN,
+  ...SESSION,
+};
