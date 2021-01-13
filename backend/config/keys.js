9481acc57c66b42f35d75aa69d4324a69dfd98dc
RitaKrasnovid <mkrasnovid@lineate.com>
2021-01-13 13:17:16 +0400

    Update client secret

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,6 +1,6 @@
 const GOOGLE_TOKEN = {
   CLIENT_ID: '189500692755-4nnlqlvge3lq0lqobi54ds85l0rosdq4.apps.googleusercontent.com',
-  CLIENT_SECRET: '24Qq2KF77ORmWFjjdY2OcbUo',
+  CLIENT_SECRET: 'iYQA5AW0pqFRCKBZ1OBrh8zb',
 };
 
 const SESSION = {
