831897d1e03c312105fa5599a7f2e97ad71ccb32
RitaKrasnovid <mkrasnovid@lineate.com>
2021-01-14 18:45:51 +0400

    Add create news button

--- zoo/src/containers/news/news.js
+++ zoo/src/containers/news/news.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
-import { getAllNews } from '../../store/actions';
+import { Link } from 'react-router-dom';
+import { getAllNews, getAuthData } from '../../store/actions';
 
 import SectionLayout from "../../components/section-layout";
 import NewsList from "../../components/news-list";
@@ -26,23 +27,35 @@ class NewsPage extends Component {
     const { dispatch } = this.props;
 
     dispatch(getAllNews());
+    dispatch(getAuthData());
   }
 
   render() {
     const { maxListSize } = this.state;
-    const { news } = this.props;
+    const { news, user, isLoggedIn } = this.props;
 
     const renderedNews = news.slice(0, maxListSize);
     let classButton = 'news__button';
+    let addButton;
 
     if(news.length === 0 || news.length === renderedNews.length) {
       classButton += ' hide';
     }
 
+    if(isLoggedIn && user.role === 'admin') {
+      addButton =
+        <Link className="news__create-button" to='/news_form'>
+          Add article
+        </Link>
+    }
+
     return (
       <div className="news">
         <SectionLayout>
-          <header className="news__header">Articles</header>
+          <header className="news__header">
+            Articles
+            {addButton}
+          </header>
           <NewsList news={renderedNews} />
           <button onClick={this.loadMore} className={classButton}>Load more</button>
         </SectionLayout>
@@ -58,6 +71,8 @@ NewsPage.defaultProps = {
 
 const mapStateToProps = state => ({
   news: state.news.getAll,
+  user: state.auth.getUser,
+  isLoggedIn: state.auth.isLoggedIn,
 });
 
 export default connect(mapStateToProps)(NewsPage);
