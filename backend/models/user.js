aef7efb15ea7ad7d56013b039a2b0a86b72cb43e
RitaKrasnovid <mkrasnovid@lineate.com>
2021-01-14 13:40:05 +0400

    Add roles to user in backend side

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,7 +1,20 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('Users', {
-    name: DataTypes.STRING,
-    googleId: DataTypes.STRING,
+    name: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    googleId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isIn: [['admin', 'visitor']],
+      },
+    },
   }, {});
 
   User.associate = function(models) {
