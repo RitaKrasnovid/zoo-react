e3ebfac5529e75c45fc380ced1558923a654c115
RitaKrasnovid <mkrasnovid@lineate.com>
2021-02-08 12:31:12 +0400

    Fix deliver.sh

--- jenkins/scripts/deliver-ui.sh
+++ jenkins/scripts/deliver-ui.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env sh
 
-echo 'The following "npm" command builds your Node.js/React application for'
-echo 'production in the local "build" directory (i.e. within the'
-echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
-echo 'correctly bundles React in production mode and optimizes the build for'
-echo 'the best performance.'
+echo 'The following "npm" command builds ui zoo for'
+echo 'production in the local "build" directory'
 set -x
+cd './zoo'
 npm run build
 set +x
 
